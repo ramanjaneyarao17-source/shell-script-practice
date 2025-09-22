529f4da1c8d5518a0573afb537842ecd81c29830
Ramanjaneyarao <ramanjaneyarao17@gmail.com>
2025-09-22 20:16:42 +0530

    even number or odd number using if condition

--- 05-installation1.sh
+++ 05-installation1.sh
@@ -3,6 +3,6 @@
 if [ $uid -eq 0 ]
     echo "running script as root"
     dnf install nginx -y
-    else
-        echo "please run the script as root"
+else
+    echo "please run the script as root"
 fi
