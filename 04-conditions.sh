043f5ba43090014c74154469eed4a5a1aada7a64
Ramanjaneyarao <ramanjaneyarao17@gmail.com>
2025-09-22 20:09:04 +0530

    even number or odd number using if condition

--- 04-conditions.sh
+++ 04-conditions.sh
@@ -6,7 +6,7 @@ echo "enter a numabr to find even or odd"
 
 read NUMBER
 
-if [$((NUMBER % 2)) -eq 0]; then
+if [ $((NUMBER % 2)) -eq 0 ]; then
     echo "$NUMBER is Even Number"
     else
     echo "$NUMBER is Odd NUmber"
