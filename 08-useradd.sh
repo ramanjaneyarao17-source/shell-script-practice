b995121732ebc3417c70ecbff89b6341d2bbc8e5
Ramanjaneyarao <ramanjaneyarao17@gmail.com>
2025-09-25 20:05:17 +0530

    script to check user exist or not

--- 08-useradd.sh
+++ 08-useradd.sh
@@ -8,7 +8,9 @@ USER_SHELL=$4
 USER_HOMEDIR=$5
 USER_CMT=$6
 
-if [ 'id $USER_NAME > /dev/null'; $? -ne 0 ]; then
+id $USER_NAME > /dev/null
+
+if [ $? -ne 0 ]; then
     echo "user $USER_NAME not existed"
 else
     echo "user $USER_NAME already existed"
